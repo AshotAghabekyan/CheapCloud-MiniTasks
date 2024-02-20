2ef251cd507160fdb0bfeff968cd1885418fae3a
Ashot Aghabekyan <129282263+AshotAghabekyan@users.noreply.github.com>
2024-02-20 18:52:54 +0400

    Delete task1 directory

--- task1/task1.js
+++
@@ -1,7 +0,0 @@
-
-
-let targetButton = document.getElementById("targetButton");
-targetButton.addEventListener("click", function() {
-    buttonText = targetButton.textContent == "Hello"? "Goodbye" : "Hello";
-    targetButton.innerHTML = buttonText;
-})
