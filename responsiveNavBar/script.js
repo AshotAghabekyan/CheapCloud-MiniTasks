170dbb70a4ade100a810452e74e5fa5eafbcda4d
Ashot <axabekyan.ashot.2003@gmail.com>
2024-02-22 17:31:16 +0400

    responsive navigation bar

---
+++ responsiveNavBar/script.js
@@ -0,0 +1,13 @@
+document.getElementById("mobileNavToggle").onclick = function() {
+    document.getElementById("myModal").style.display = "block";
+};
+
+document.getElementById("closeMobileNavBar").addEventListener("click", function() {
+    document.getElementById("myModal").style.display = "none";
+}) 
+
+window.onclick = function(event) {
+    if (event.target === document.getElementById("myModal")) {
+        document.getElementById("myModal").style.display = "none";
+    }
+};
