5aa8ddec6da44eb68f87f02c05ecb0c16a292112
Ashot <axabekyan.ashot.2003@gmail.com>
2024-02-22 13:44:44 +0400

    basic slider

---
+++ basicSlider/script.js
@@ -0,0 +1,38 @@
+
+
+let imgArray = [
+        "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
+        "https://images.pexels.com/photos/1402787/pexels-photo-1402787.jpeg?auto=compress&cs=tinysrgb&w=400",
+        "https://img.freepik.com/premium-photo/sunset-sea-created-with-generative-ai-tools_732031-3446.jpg?size=626&ext=jpg&ga=GA1.1.1413502914.1697241600&semt=ais",
+]
+
+
+
+let sliderDiv = document.getElementById("slider");
+let position = 0;
+sliderDiv.style.background = `url(${imgArray[position]})`;
+sliderDiv.style.backgroundSize = "cover";
+sliderDiv.style.backgroundRepeat = "no-repeat"  
+
+
+let nextButton = document.getElementById("next");
+nextButton.addEventListener("click", function() {
+        if (position == imgArray.length - 1) {
+            position = 0;        
+        } else {
+            ++position
+        }
+        sliderDiv.style.background = `url(${imgArray[position]})`;
+})
+
+
+
+let previousButton = document.getElementById("previous");
+previousButton.addEventListener("click", function() {
+        if (position == 0) {
+                position = imgArray.length - 1;
+        } else {
+                --position;
+        }
+        sliderDiv.style.background = `url(${imgArray[position]})`;
+})
