b33e6b148f8ee7fad775b889a1ba498b4364e20a
Ashot <axabekyan.ashot.2003@gmail.com>
2024-02-22 16:55:10 +0400

    changed basic slider

--- basicSlider/script.js
+++ basicSlider/script.js
@@ -1,38 +1,40 @@
 
 
 let imgArray = [
-        "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-        "https://images.pexels.com/photos/1402787/pexels-photo-1402787.jpeg?auto=compress&cs=tinysrgb&w=400",
-        "https://img.freepik.com/premium-photo/sunset-sea-created-with-generative-ai-tools_732031-3446.jpg?size=626&ext=jpg&ga=GA1.1.1413502914.1697241600&semt=ais",
-]
-
-
-
-let sliderDiv = document.getElementById("slider");
+    "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
+    "https://images.pexels.com/photos/1402787/pexels-photo-1402787.jpeg?auto=compress&cs=tinysrgb&w=400",
+    "https://img.freepik.com/premium-photo/sunset-sea-created-with-generative-ai-tools_732031-3446.jpg?size=626&ext=jpg&ga=GA1.1.1413502914.1697241600&semt=ais",
+];
+    
+let sliderImage = document.getElementById("sliderImage");
 let position = 0;
-sliderDiv.style.background = `url(${imgArray[position]})`;
-sliderDiv.style.backgroundSize = "cover";
-sliderDiv.style.backgroundRepeat = "no-repeat"  
+    
+function updateSlider() {
+    sliderImage.src = imgArray[position];
+}
 
-
+updateSlider();
+    
+   
+    
 let nextButton = document.getElementById("next");
 nextButton.addEventListener("click", function() {
-        if (position == imgArray.length - 1) {
-            position = 0;        
-        } else {
-            ++position
-        }
-        sliderDiv.style.background = `url(${imgArray[position]})`;
-})
-
-
+    if (position === imgArray.length - 1) {
+        position = 0;
+    } else {
+        ++position;
+    }
+    updateSlider();
+});
+    
 
 let previousButton = document.getElementById("previous");
 previousButton.addEventListener("click", function() {
-        if (position == 0) {
-                position = imgArray.length - 1;
-        } else {
-                --position;
-        }
-        sliderDiv.style.background = `url(${imgArray[position]})`;
-})
+    if (position === 0) {
+        position = imgArray.length - 1;
+    } else {
+        --position;
+    }
+    updateSlider();
+});
+    
